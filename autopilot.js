5be7113fc39872be7af209d851d9ddf15ef1a002
Myles Bennett <myles.bennett@hotmail.com>
2019-03-24 23:42:34 -0400

    defined drive , drop off, and act functions

--- autopilot.js
+++ autopilot.js
@@ -37,3 +37,38 @@ function fillUpGas(car) {
 function getGasDisplay(gas_amount) {
     return `${gas_amount}%`;
 }
+
+function drive(car, city_distance) {
+    if (car.gas < city_distance) {
+        return fillUpGas(car);
+    }
+
+    car.city = getDestination(car);
+    car.gas -= city.distance;
+    return `Drove to ${car.city}. Remaining gas: ${getGasDisplay(car.gas)}`
+}
+
+function dropOffPassengers(car) {
+    previousPassengers = car.passengers;
+    car.passengers = 0;
+    return `Dropped off ${previousPassengers} passengers`
+}
+
+function act(car) {
+    distanceBetweenCities = 50;
+
+    if (car.gas < 20) {
+        return fillUpGas(car);
+    } else if (car.passengers < 3) {
+        return pickUpPassenger(car);
+    } else if (car.gas < distanceBetweenCities) {
+        return fillUpGas(car);
+    }
+
+    droveTo = drive(car, distanceBetweenCities);
+    passengersDropped = dropOffPassengers(car);
+    return `${droveTo} ${passengersDropped}`
+}
+
+
+
